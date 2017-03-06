bd5dd13435a638b9cb8ae2794c08d82f44a6789b
dabeles <dabeles@akamai.com>
2017-03-06 08:57:58 +0200

    Added run

--- run.py
+++ run.py
@@ -5,12 +5,10 @@ from gevent import monkey
 # need to patch sockets to make requests async
 monkey.patch_all()
 
-
 if __name__ == "__main__":
 
-    print("Started gevent WSGI server")
+    print("Started JSShell server ...")
     # use gevent WSGI server instead of the Flask
 
     http = WSGIServer(('', 5000), app.wsgi_app)
-    # TODO gracefully handle shutdown
     http.serve_forever()
