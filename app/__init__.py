207069d7d95e966ba090948698e5fefa6e37d0eb
Den1al <abeles22@gmail.com>
2017-03-07 15:54:02 +0200

    Added the `db.create_all` function call to ensure the database is up and runnig initally when the application starts, regardless if the user created it or not.

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
 app.config.from_object('app.config.DevelopmentConfig')
 
 db = SQLAlchemy(app)
 
 from app import views, models
+
+db.create_all()
