48e90ade1ae0e3ad044b3e71760875202172bc91
Den1al <abeles22@gmail.com>
2017-04-24 09:57:20 +0300

    Updated the CORS settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('app.config.DevelopmentConfig')
+
+cors = CORS(app)
 
 db = SQLAlchemy(app)
 
--- app/preflight_scripts.py
+++ app/preflight_scripts.py
@@ -3,6 +3,6 @@
 pf_scripts = [
     "window",
     "window.document",
-    "var l = {\"availHeight\" : window.screen.availHeight,\"availLeft\" : window.screen.availLeft,\"availTop\" : window.screen.availTop, \"availWidth\" : window.screen.availWidth, \"colorDepth\": window.screen.colorDepth, \"height\" : window.screen.height, \"orientation\" : window.screen.orientation, \"ScreenOrientation\" : window.screen.ScreenOrientation, \"pixelDepth\" : window.screen.pixelDepth, \"width\" : window.screen.width};l",
-    "var l = [];for(var i=0;i<navigator.plugins.length;i++){ l.push(navigator.plugins[i].name) };l"
+    "var temp = {\"availHeight\" : window.screen.availHeight,\"availLeft\" : window.screen.availLeft,\"availTop\" : window.screen.availTop, \"availWidth\" : window.screen.availWidth, \"colorDepth\": window.screen.colorDepth, \"height\" : window.screen.height, \"orientation\" : window.screen.orientation, \"ScreenOrientation\" : window.screen.ScreenOrientation, \"pixelDepth\" : window.screen.pixelDepth, \"width\" : window.screen.width};temp",
+    "var temp = [];for(var i=0;i<navigator.plugins.length;i++){ l.push(navigator.plugins[i].name) };temp"
 ]
