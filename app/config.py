8ccf6734883713ded74e8d2778dcc046e0bc915d
Daniel Abeles <abeles22@gmail.com>
2018-12-16 16:50:06 +0200

    .

---
+++ app/config.py
@@ -0,0 +1,23 @@
+
+class Config(object):
+    """
+    Configuration base, for all environments.
+    """
+    DEBUG = True
+    DATABASE_URI = 'sqlite:///application.db'
+    SECRET_KEY = "LKHSDNIOUTY&*(^87bdasdasdasv6*&BGSUYDS"
+    CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    PORT = 8080
+    HOST = '0.0.0.0'
+    URL = 'http://localhost/'
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///application.db'
