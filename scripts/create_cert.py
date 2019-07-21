78be9b520a1eec6de613015b04736f0576a3a9ba
Den1al <me@eval.co.il>
2019-07-21 16:18:39 +0000

    added HTTPS support

---
+++ scripts/create_cert.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from subprocess import call
+
+parser = ArgumentParser('')
+parser.add_argument('domain', help='the domain to create the cert for')
+parser.add_argument('-e', '--email', help='the email to register with', default='me@<domain>')
+
+
+def main():
+    args = parser.parse_args()
+
+    if not args.email:
+        args.email = f'me@{args.domain}'
+
+    call([
+        'sudo', 'certbot', 
+        'certonly', '--standalone', 
+        '--preferred-challenge', 'dns',
+        '-d', args.domain,
+        '-m', args.email,
+        '--agree-tos'
+    ])
+
+if __name__ == "__main__":
+    main()
+
--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
+# from OpenSSL import SSL
 
 from common.config import read_config
 from common.models import db
-
 
 config = read_config()
 app = Flask(__name__)
@@ -25,8 +25,20 @@ app.register_blueprint(main_bp)
 def start_api_server() -> None:
     """ Starts the web server """
 
+    domain_name = config.get('DOMAIN', '')
+    lets_encrypt_base_path = f'/etc/letsencrypt/live/{domain_name}/'
+
+    # ssl_context = SSL.Context(SSL.TLSv1_2_METHOD)
+    # ssl_context.use_privatekey_file(lets_encrypt_base_path + 'privkey.pem')
+    # ssl_context.use_certificate_chain_file(lets_encrypt_base_path + 'fullchain.pem')
+    # ssl_context.use_certificate_file(lets_encrypt_base_path + 'cert.pem')
+
     app.run(
         host=config.get('HOST', 'localhost'),
         port=config.get('PORT', 5000),
-        debug=config.get('DEBUG', False)
+        debug=config.get('DEBUG', False),
+        ssl_context=(
+            lets_encrypt_base_path + 'cert.pem',
+            lets_encrypt_base_path + 'privkey.pem'
+        )
     )
